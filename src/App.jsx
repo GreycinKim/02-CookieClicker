218f7f37e99854e2a0528f52b438e2a380e4d5a1
Greycin Kim <kimgreycin@gmail.com>
2025-04-04 12:25:20 -0600

    Added Power upp

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,53 @@
 import { useState, useEffect } from 'react'
+import Header from './components/Header.jsx'
 import cookie from './assets/Cookie.png'
 import './App.css'
+
 
 function App() {
     const [count, setCount] = useState(0)
     const [basePower, setPower] = useState(1)
+    const [showMessage, setShowMessage] = useState(false);
 
     const resetCount = () => {
         setCount(0);
         setPower(1);
     };
 
-    // Power-up when score reaches 25
+    // When Count is greater than 0 AND count mod 100 = 0 run
     useEffect(() => {
-        if (count >= 25 && basePower === 1) {
-            setPower(2);
+        if (count > 0 && count % 100 === 0) {
+            setPower(prev => prev * 2)
+            setShowMessage(true) // SHOW message
+            setTimeout(() => setShowMessage(false), 2000)
         }
-    }, [count, basePower]);
+    },[count])
 
     const handleClick = () => {
         setCount(prev => prev + basePower);
     };
 
     return (
-        <>
-            <h1>Cookie POWER: {basePower}x</h1>
-            <h2>Click ME!!!</h2>
+        <div className="center-wrapper">
+            {showMessage && (
+                <div className="power-up-overlay">
+                    Power Up!
+                </div>
+            )}
 
-            <div>
-                <button onClick = {handleClick}>
-                    <img src={cookie} className="logo react" alt="Cookie Image" />
+            <Header basePower={basePower} />
+            <h2>Score: {count}</h2>
+
+            <div className="cookie-wrapper">
+                <button onClick={handleClick}>
+                    <img src={cookie} className="logo" alt="Cookie Image" />
                 </button>
             </div>
 
-            <h2>Score: {count}</h2>
             <button onClick={resetCount}>Reset</button>
+        </div>
 
-    </>
-  )
+    )
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header({basePower}){
+    return(
+        <>
+            <h1>Cookie POWER: {basePower}x</h1>
+        </>
+    );
+}
